808c6e26029048da0ab016981c6078721ec59126
maq-ahmad-20 <Maqboolahmad081@gmail.com>
2023-06-26 02:17:24 +0530

    deleteuserafteraddingcommit

--- storedata.js
+++ storedata.js
@@ -1,7 +1,7 @@
 
 
  
-// var form = document.getElementById('form');
+
 
 // document.addEventListener('submit' , (e)=>{
 //       e.preventDefault();
@@ -49,11 +49,13 @@
           
           
 //     });
-  
+
+var bookeduser = document.getElementById('get-booked-user');
+var form = document.getElementById('sign-up-form');
 
     // to not override the storage
-     
-    document.addEventListener('submit' , (e)=>{
+    
+    form.addEventListener('submit' , (e)=>{
         e.preventDefault();
       
         var inputs = document.getElementsByTagName('input');
@@ -65,9 +67,47 @@
        
         var object = JSON.stringify(setObj(arrInput));
         localStorage.setItem(arrInput[1] , object);
+      
         
+        var div = document.createElement('div');
+         div.className = 'after-submit-display';
+     
+         var h4 = document.createElement('h4')
+          h4.style.display = 'inline';
+      
+         h4.appendChild(document.createTextNode("User signed up : " + arrInput[1]));
+         div.appendChild(h4);
+         
+
+       
+
+         var button = document.createElement('button');
+         button.className = 'delete-button';
+         button.id = arrInput[1]; // keeping id of button as same as key to local storage
+         
+         button.appendChild(document.createTextNode("Delete User"));
+        button.style.margin = '0 auto auto 20px';
+        button.style.display = 'inline';
+     
+        div.appendChild(button);
+        bookeduser.appendChild(div);
+    });
+
+    
+    bookeduser.addEventListener('click' , (e)=>{
+        e.preventDefault();
+       // console.log(e.target.previousElementSibling.textContent); printing h4 tag text previous to button tag
+      
         
-  });
+        var buttonid = e.target.id;
+        //console.log(buttonid);
 
-  
+        var removebutton = document.getElementById(buttonid);
+        //console.log(removebutton.parentElement);
+       // var key = e.target.previousElementSibling.textContent; get text of h4  as button  prev sibbling is h4 
+        localStorage.removeItem(buttonid); // passing key as buton id because i have added arr[1] in local and arr[1] as id to the button
+        removebutton.parentElement.remove();
+})
+           
 
+    
