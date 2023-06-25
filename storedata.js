93ce726feaec07e0d1d4b6ed972217d8ef4570a9
maq-ahmad-20 <Maqboolahmad081@gmail.com>
2023-06-25 23:54:07 +0530

    thirdcommit

--- storedata.js
+++ storedata.js
@@ -1,20 +1,51 @@
 
 
  
-var form = document.getElementById('form');
+// var form = document.getElementById('form');
+
+// document.addEventListener('submit' , (e)=>{
+//       e.preventDefault();
+    
+//       var inputs = document.getElementsByTagName('input');
+     
+//        for(let i=0;i<inputs.length;i++){
+        
+//         if(inputs[i].id !="submit"){
+//         var value = inputs[i].value;
+//         var key = "userInput - "+inputs[i].id;
+//         console.log(value);
+//         localStorage.setItem(key,value);
+      
+//        }
+//     }  
+// });
+
+
+
+ 
+ function setObj(arr){
+     return{
+        "userName" : arr[0],
+        "userEmail" : arr[1],
+        "userPhoneNo" : arr[2],
+        "userdate" : arr[3],
+        "usertime": arr[4]
+     
+     }
+ }
 
 document.addEventListener('submit' , (e)=>{
-      e.preventDefault();
-    
-      var inputs = document.getElementsByTagName('input');
-     
-       for(let i=0;i<inputs.length;i++){
+          e.preventDefault();
         
-        if(inputs[i].id !="submit"){
-        var value = inputs[i].value;
-        var key = "userInput - "+inputs[i].id;
-        console.log(value);
-        localStorage.setItem(key,value);
-       }
-    }  
-});
+          var inputs = document.getElementsByTagName('input');
+         
+           let arrInput = [];
+           for(let i=0;i<inputs.length;i++){
+            arrInput[i] = inputs[i].value;
+           }
+         
+          var object = JSON.stringify(setObj(arrInput));
+          localStorage.setItem("submitObject" , object);
+          
+          
+    });
